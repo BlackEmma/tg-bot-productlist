1ec007c2d09d58fe3ac2eb37f09db13d8585c92c
Ekaterina Ekimova <psich.kawai@gmail.com>
2023-01-20 00:40:36 +0300

    add model&migration for db

---
+++ db/migrations/20230119205749-create-product.js
@@ -0,0 +1,36 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Products');
+  },
+};
---
+++ db/models/product.js
@@ -0,0 +1,47 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User }) {
+      Product.belongsTo(User, { foreignKey: 'user_id' });
+    }
+  }
+  Product.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    user_id: {
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      type: DataTypes.INTEGER,
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'Product',
+    tableName: 'Products',
+  });
+  return Product;
+};
